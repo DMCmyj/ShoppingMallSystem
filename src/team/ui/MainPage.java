c40b66a021686e2a1822988ab5ac7b8c8a92ff9b
马永杰 <1846569723@qq.com>
2020-06-18 11:39:19 +0800

    完成主界面左侧按钮的制作

--- src/team/ui/MainPage.java
+++ src/team/ui/MainPage.java
@@ -14,7 +14,7 @@ public class MainPage extends JFrame {
     public MainPage(){
         window = new JFrame("商品管理系统登录页面");
         window.setLayout(null);
-        window.setSize(400,300);
+        window.setSize(600,400);
         //设置窗口在屏幕居中
         window.setLocationRelativeTo(null);
 
@@ -25,6 +25,18 @@ public class MainPage extends JFrame {
         goods_buy = new JButton("商品买入");
         goods_buy.setBounds(20,80,100,50);
         window.add(goods_buy);
+
+        add_goods = new JButton("添加商品");
+        add_goods.setBounds(20,140,100,50);
+        window.add(add_goods);
+
+        delete_goods = new JButton("删除商品");
+        delete_goods.setBounds(20,200,100,50);
+        window.add(delete_goods);
+
+        change_goods = new JButton("修改商品");
+        change_goods.setBounds(20,140,100,50);
+        window.add(change_goods);
 
         window.setVisible(true);
     }
