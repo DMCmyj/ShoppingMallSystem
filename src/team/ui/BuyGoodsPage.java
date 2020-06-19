6e94a1a6b2f8c207211380eb5e926e2ef558c6d5
马永杰 <1846569723@qq.com>
2020-06-19 13:48:09 +0800

    完成商品买入功能

---
+++ src/team/ui/BuyGoodsPage.java
@@ -0,0 +1,79 @@
+package team.ui;
+
+import team.db.LinkDB;
+import team.sys.Goods;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.util.Objects;
+
+public class BuyGoodsPage {
+    JLabel goods_name;
+    JLabel goods_num;
+    JLabel sell_num_tip;
+    JTextField goodsSellNum;
+
+    JButton submit;
+    JFrame window;
+
+    public BuyGoodsPage(Goods goods){
+        window = new JFrame("商品出售页面");
+        window.setLayout(null);
+        window.setSize(400,360);
+        window.setLocationRelativeTo(null);
+
+        ResultSet resultSet = LinkDB.getAllGoods();
+        goods_name = new JLabel("商品名称:" + goods.getGoods_name());
+        goods_name.setBounds(50,20,400,50);
+        goods_name.setFont(new Font("楷体",Font.PLAIN,25));
+        window.add(goods_name);
+
+        goods_num = new JLabel("商品现有数量：" + goods.getGoods_num());
+        goods_num.setFont(new Font("楷体",Font.PLAIN,25));
+        goods_num.setBounds(50,60,400,50);
+        window.add(goods_num);
+
+        sell_num_tip = new JLabel("请输入需要买入的数量：");
+        sell_num_tip.setFont(new Font("楷体",Font.PLAIN,20));
+        sell_num_tip.setBounds(50,120,400,50);
+        window.add(sell_num_tip);
+//绘制水平线
+        DrawLinePanel drawLinePanel = new DrawLinePanel();
+        drawLinePanel.setBounds(0,120,window.getWidth(),20);
+        window.add(drawLinePanel);
+
+        goodsSellNum = new JTextField("0");
+        goodsSellNum.setBounds(50,170,260,30);
+        window.add(goodsSellNum);
+
+        submit = new JButton("确认买入");
+        submit.setBounds(50,220,270,50);
+        submit.setFont(new Font("楷体",Font.PLAIN,25));
+//        出售逻辑
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!Objects.equals(goodsSellNum.getText(), "")){
+                    int buyNum = Integer.parseInt(goodsSellNum.getText());
+                    if(buyNum>=0){
+                        goods.setGoods_num(goods.getGoods_num()+buyNum);
+                        if(LinkDB.SetGoodsNum(goods)){
+                            JOptionPane.showMessageDialog(null,"买入成功,花费为"+buyNum*goods.getGoods_price()+"元","警告",JOptionPane.PLAIN_MESSAGE);
+                            window.setVisible(false);
+                            MainPage mainPage = new MainPage();
+                        }
+                    }else {
+                        JOptionPane.showMessageDialog(null,"输入错误","警告",JOptionPane.PLAIN_MESSAGE);
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(null,"数量不能为空","警告",JOptionPane.PLAIN_MESSAGE);
+                }
+            }
+        });
+        window.add(submit);
+        window.setVisible(true);
+    }
+}
--- src/team/ui/MainPage.java
+++ src/team/ui/MainPage.java
@@ -20,12 +20,15 @@ public class MainPage extends JFrame {
     JButton delete_goods;
     JButton change_goods;
 
+    Font font_button = new Font("楷体", Font.PLAIN,18);
+
     public MainPage(){
         window = new JFrame("商品管理系统登录页面");
         window.setLayout(null);
-        window.setSize(1100,600);
+        window.setSize(1120,400);
         //设置窗口在屏幕居中
         window.setLocationRelativeTo(null);
+        window.setResizable(false);
 
         ResultSet resultSet = LinkDB.getAllGoods();
 
@@ -36,19 +39,24 @@ public class MainPage extends JFrame {
                 return false;
             }
         };
+
 //      数据表外观设置
         jTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         JScrollPane jScrollPane = new JScrollPane(jTable);
         jTable.setRowHeight(30);
-        jScrollPane.setBounds(140,20,900,300);
+        jScrollPane.setBounds(180,20,900,300);
         jTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         window.getContentPane().add(jScrollPane,BorderLayout.CENTER);
+//        默认选择第一行，需要在表格初始化完成后才可操作
+        jTable.setRowSelectionInterval(0,0);
+
 
 
 
 //      商品出售按钮
         goods_sold = new JButton("商品出售");
-        goods_sold.setBounds(20,20,100,50);
+        goods_sold.setBounds(20,20,140,50);
+        goods_sold.setFont(font_button);
         goods_sold.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -67,11 +75,28 @@ public class MainPage extends JFrame {
         window.add(goods_sold);
 
         goods_buy = new JButton("商品买入");
-        goods_buy.setBounds(20,80,100,50);
+        goods_buy.setBounds(20,80,140,50);
+        goods_buy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num = Integer.parseInt((String) jTable.getValueAt(jTable.getSelectedRow(),4));
+                BuyGoodsPage buyGoodsPage = new BuyGoodsPage(
+                        new Goods(
+                                (String) jTable.getValueAt(jTable.getSelectedRow(),1),
+                                Double.parseDouble((String) jTable.getValueAt(jTable.getSelectedRow(),3)),
+                                (String) jTable.getValueAt(jTable.getSelectedRow(),2),
+                                num
+                        )
+                );
+                window.setVisible(false);
+            }
+        });
+        goods_buy.setFont(font_button);
         window.add(goods_buy);
 
         add_goods = new JButton("添加商品");
-        add_goods.setBounds(20,140,100,50);
+        add_goods.setBounds(20,140,140,50);
+        add_goods.setFont(font_button);
         add_goods.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -82,11 +107,13 @@ public class MainPage extends JFrame {
         window.add(add_goods);
 
         delete_goods = new JButton("删除商品");
-        delete_goods.setBounds(20,200,100,50);
+        delete_goods.setFont(font_button);
+        delete_goods.setBounds(20,200,140,50);
         window.add(delete_goods);
 
         change_goods = new JButton("修改商品");
-        change_goods.setBounds(20,140,100,50);
+        change_goods.setFont(font_button);
+        change_goods.setBounds(20,260,140,50);
         window.add(change_goods);
 
         window.add(jScrollPane);
