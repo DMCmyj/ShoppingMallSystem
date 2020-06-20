ec0529a3aafd552c99125359fcc5be62f00a629e
马永杰 <1846569723@qq.com>
2020-06-20 12:51:59 +0800

    完成了商品列表的查询功能

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,14 @@
 import team.db.LinkDB;
+import team.sys.Goods;
 import team.ui.LoginPage;
+import team.ui.MainPage;
 import team.ui.RegistPage;
+import team.ui.SellGoodsPage;
 
 public class Program {
     public static void main(String[] args) {
         LinkDB linkDB = new LinkDB();
-        LoginPage loginPage = new LoginPage();
+//        LoginPage loginPage = new LoginPage();
+        MainPage mainPage = new MainPage();
     }
 }
